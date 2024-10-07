caa8082746070d3fb6478a95eeac0ee707c2604e
alejandroava <alejandroalzate.ava@gmail.com>
2024-10-07 15:18:12 -0500

    Add user entity

--- src/main/java/com/logistic_warehouse/domain/entities/UserEntity.java
+++ src/main/java/com/logistic_warehouse/domain/entities/UserEntity.java
@@ -1,4 +1,59 @@
 package com.logistic_warehouse.domain.entities;
 
-public class UserEntity {
+import com.logistic_warehouse.utils.enu.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity(name = "users")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique = true)
+    private String username;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/logistic_warehouse/utils/enu/Role.java
@@ -0,0 +1,6 @@
+package com.logistic_warehouse.utils.enu;
+
+public enum Role {
+    ADMIN,
+    CARRIER
+}
